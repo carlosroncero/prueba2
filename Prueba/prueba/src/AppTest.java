55f2db7fa19ccba313196ae32631840cacb77abf
croncero <croncero@yahoo.es>
2023-07-05 20:56:17 +0200

    segunda modificación

--- Prueba/prueba/src/AppTest.java
+++ Prueba/prueba/src/AppTest.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import App.java;
 
@@ -9,7 +11,7 @@ public class AppTest {
         // assertEquals(5, aa.sumar2(2, 3));
         App bb = new App();
         assertEquals(5, bb.sumar(2, 3));
-
+        assertTrue(bb.sumar(5,4)==9);
     }
 
 }
